0b94159a0edf47e8289f3ce3025b540dfac66666
Amelss <ameleyka@gmail.com>
2022-05-11 21:26:44 +0100

    moving of html file and rounding of temperature

--- src/script.js
+++ src/script.js
@@ -14,16 +14,16 @@ let weather = {
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, humidity } = (data.main);
         const { speed } = data.wind;
         document.querySelector(".city").innerText = name
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = (Math.round (temp)) + "°C";
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png"
         document.querySelector(".description").innerText = description
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
         document.querySelector(".wind").innerText = "Wind: " + speed + " mp/h"
         document.querySelector(".weather").classList.remove(".invisible")
-
+        console.log(Math.round(temp));
     },
     
     search: function () {
