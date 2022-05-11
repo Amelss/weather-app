88b31d3c43ca92001623d84cec7559c1ddc002dd
Amelss <ameleyka@gmail.com>
2022-05-11 19:59:21 +0100

    finished styling and added to the api

--- src/script.js
+++ src/script.js
@@ -16,13 +16,13 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, humidity, temp, speed);
-        document.querySelector(".city").innerText = "Weather in " + name
+        document.querySelector(".city").innerText = name
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + "@2x.png"
         document.querySelector(".description").innerText = description
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"
         document.querySelector(".wind").innerText = "Wind: " + speed + " mp/h"
+        document.querySelector(".weather").classList.remove(".invisible")
 
     },
     
@@ -36,3 +36,11 @@ let weather = {
 document.querySelector(".search button").addEventListener('click', function () {
     weather.search()
 })
+
+document.querySelector(".search-bar").addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        weather.search()
+    }
+})
+
+weather.fetchWeather("London")
