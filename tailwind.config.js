09ad34db8af3405fbf8c024ab23c7feab371fb9a
Amelss <ameleyka@gmail.com>
2022-05-11 17:22:16 +0100

    installed tailwind, styling of weather interface

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'montserrat': ['montserrat']
+      },
+      backgroundImage: {
+        'hero': "url ('/public/images/weather-hero.jpg)"
+      }
+    },
+  },
+  plugins: [],
+};
